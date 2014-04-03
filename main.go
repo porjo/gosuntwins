a95e028f4643208c3e1160bcbdba3f3ee8dfc69f
Porjo <porjo38@yahoo.com.au>
2014-04-04 08:55:18 +1000

    Tidy up logging. Log to file

--- main.go
+++ main.go
@@ -2,59 +2,95 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"time"
 
 	"github.com/tarm/goserial"
 )
 
+type Output struct {
+	Temp     uint16
+	Unknown1 uint16
+	VDC      uint16
+	CurrentE uint16
+	Unknown2 uint16
+	TodayE   uint16
+	I        uint16
+	VAC      uint16
+	Freq     uint16
+	PAC      uint16
+}
+
 var outbuffer bytes.Buffer
 var inbuffer bytes.Buffer
 
 const sourceaddr byte = 1
-
 var destaddr byte = 0
-
 const headerlen int = 7
 
 var serialPort string = "/dev/ttyUSB0"
+var dataFile string = "data.csv"
+
+var debug bool = true
+var results map[string]float32 = make(map[string]float32)
 
 func main() {
 	c := &serial.Config{Name: serialPort, Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
-		log.Fatal(err)
+		mylogln(err)
+		return
 	}
 
 	err = initInverter(s)
 	if err != nil {
-		log.Println(err)
-		return
-	}
-	err = readInverter(s)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	err = outputInverter()
-	if err != nil {
-		log.Println(err)
+		mylogln(err)
 		return
 	}
 
+	go func() {
+		for {
+			err = readInverter(s)
+			if err != nil {
+				mylogln(err)
+				return
+			}
+			err = outputInverter()
+			if err != nil {
+				mylogln(err)
+				return
+			}
+			time.Sleep(time.Second * 30)
+		}
+	}()
+}
+
+func mylogf(format string, args ...interface{}) {
+	if debug {
+		log.Printf(format, args...)
+	}
+}
+
+func mylogln(args ...interface{}) {
+	if debug {
+		log.Println(args...)
+	}
 }
 
 func initInverter(s io.ReadWriteCloser) error {
 	var control byte = 0x30
-	var function byte = 0x44
+	var function byte = 0x44 //Initialize inverter
 	err := createCommand(control, function, nil)
 	if err != nil {
 		return err
 	}
-	log.Printf("outbuffer %#v\n", outbuffer.Bytes())
+
+	mylogf("Initializing inverter: => %X\n", outbuffer.Bytes())
 
 	n, err := s.Write(outbuffer.Bytes())
 	if err != nil {
@@ -62,14 +98,16 @@ func initInverter(s io.ReadWriteCloser) error {
 	}
 
 	// wait before sending next command
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 500)
 
-	function = 0x40
+	function = 0x40 //Identify inverter
 	err = createCommand(control, function, nil)
 	if err != nil {
 		return err
 	}
-	log.Printf("outbuffer %#v\n", outbuffer.Bytes())
+
+	mylogf("Identifying inverter: =>  %X\n", outbuffer.Bytes())
+
 	n, err = s.Write(outbuffer.Bytes())
 	if err != nil {
 		return err
@@ -83,32 +121,37 @@ func initInverter(s io.ReadWriteCloser) error {
 		return fmt.Errorf("Too few bytes read. Expected >= %d, got %d\n", headerlen, n)
 	}
 
-	inbuffer.Write(inbuf[:n])
-
 	// wait before sending next command
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 500)
 
-	log.Printf("inbuf %#v, n=%d\n", inbuf[:n], n)
-	function = 0x41
+	mylogf("Read data: <=  %X\n", inbuf[:n])
+
+	function = 0x41 // Register inverter
 	// get the serial number from the response
 	//serno := make([]byte,inbuf[6])
 	serno := inbuf[headerlen:(int(inbuf[6]) + headerlen + 1)]
-	log.Printf("headerlen %d inbuf6 %#v\n", headerlen, inbuf[6])
+
+	//mylogf("headerlen %d inbuf6 %#v\n", headerlen, inbuf[6])
 
 	// set the device id
 	serno[inbuf[6]] = 1
-	log.Printf("serno %#v len %d\n", serno, len(serno))
+
+	//mylogf("serno %#v len %d\n", serno, len(serno))
+
 	// now register the inverter as device id 1
 	err = createCommand(control, function, serno[:inbuf[6]+1])
 	if err != nil {
 		return err
 	}
-	log.Printf("outbuffer %#v\n", outbuffer.Bytes())
+
+	mylogf("Register inverter: =>  %X\n", outbuffer.Bytes())
+
 	n, err = s.Write(outbuffer.Bytes())
 	if err != nil {
 		return err
 	}
 
+	inbuf = nil
 	n, err = s.Read(inbuf)
 	if err != nil {
 		return err
@@ -116,36 +159,50 @@ func initInverter(s io.ReadWriteCloser) error {
 	if n < headerlen {
 		return fmt.Errorf("Too few bytes read. Expected >= %d, got %d\n", headerlen, n)
 	}
-	log.Printf("inbuf %#v, n=%d\n", inbuf[:n], n)
+
+	mylogf("Read data: <=  %X\n", inbuf[:n])
 
 	return nil
 }
 
 func readInverter(s io.ReadWriteCloser) error {
 	var control byte = 0x31
-	var function byte = 0x42
+	var function byte = 0x42 //Get Dynamic data
 	destaddr = 1
 	err := createCommand(control, function, nil)
 	if err != nil {
 		return err
 	}
-	log.Printf("outbuffer %#v\n", outbuffer.Bytes())
+
+	mylogf("Requesting data: => %X\n", outbuffer.Bytes())
+
 	n, err := s.Write(outbuffer.Bytes())
 	if err != nil {
 		return err
 	}
-	inbuf := make([]byte, 256)
-	n, err = s.Read(inbuf)
-	if err != nil {
-		return err
+	var inbuf []byte
+	bytecount := 0
+	for {
+		tmpbuf := make([]byte, 256)
+		n, err = s.Read(tmpbuf)
+		inbuf = append(inbuf, tmpbuf[:n]...)
+		bytecount += n
+		mylogf("Read %d bytes\n", bytecount)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
 	}
-	if n < headerlen {
+	if bytecount < headerlen {
 		return fmt.Errorf("Too few bytes read. Expected >= %d, got %d\n", headerlen, n)
 	}
 
-	log.Printf("inbuf %#v, n=%d\n", inbuf[:n], n)
+	//mylogf("inbuf %X\n", inbuf)
+
 	inbuffer.Reset()
-	inbuffer.Write(inbuf[:n])
+	inbuffer.Write(inbuf)
 
 	return nil
 }
@@ -154,23 +211,35 @@ func outputInverter() error {
 	if len(inbuffer.Bytes()) < headerlen+20 {
 		return fmt.Errorf("Too few bytes read. Expected >= %d, got %d\n", headerlen+20, len(inbuffer.Bytes()))
 	}
-	buf := inbuffer.Bytes()[headerlen:]
+	data := inbuffer.Bytes()[headerlen:]
 
-	temp := float32((buf[0]<<8)+buf[1]) / 10.0
-	todayE := float32((buf[2]<<8)+buf[3]) / 100.0
-	VDC := float32((buf[4]<<8)+buf[5]) / 10.0
-	I := float32((buf[6]<<8)+buf[7]) / 10.0
-	VAC := float32((buf[8]<<8)+buf[9]) / 10.0
-	freq := float32((buf[10]<<8)+buf[11]) / 100.0
-	currE := float32((buf[12] << 8) + buf[13])
-	unk1 := float32((buf[14] << 8) + buf[15])
-	unk2 := float32((buf[16] << 8) + buf[17])
-	totE := float32((buf[18]<<8)+buf[19]) / 10.0
+	b := bytes.NewBuffer(data)
+	output := Output{}
+	err := binary.Read(b, binary.BigEndian, &output)
+	if err != nil {
+		return err
+	}
 
-	fmt.Printf("temp:%.3f TodayE:%.3f VDC:%.3f I:%.3f VAC:%.3f Freq:%.3f CurrE:%.3f unk1:%.3f unk2:%.3f TotE:%.3f\n", temp, todayE, VDC, I, VAC, freq, currE, unk1, unk2, totE)
+	results["Temperature"] = float32(output.Temp) / 10.0
+	results["KW today"] = float32(output.TodayE) / 100.0
+	results["Volts DC"] = float32(output.VDC) / 10.0
+	results["Current"] = float32(output.I) / 10.0
+	results["Volts AC"] = float32(output.VAC) / 10.0
+	results["Frequency"] = float32(output.Freq) / 100.0
+	results["KW now"] = float32(output.CurrentE) / 10.0
+	results["Unknown 1"] = float32(output.Unknown1)
+	results["Unknown 2"] = float32(output.Unknown2)
+	results["PV AC"] = float32(output.PAC) / 10.0
 
+	resultsStr := fmt.Sprintf("%s, ", time.Now())
+	for k, v := range results {
+		resultsStr += fmt.Sprintf("%s:%f, ", k, v)
+	}
+	resultsStr += "\n"
+
+	mylogf(resultsStr)
+	writeLine(dataFile, resultsStr)
 	return nil
-
 }
 
 func createCommand(control byte, function byte, data []byte) error {
@@ -194,7 +263,7 @@ func createCommand(control byte, function byte, data []byte) error {
 
 	check1, check2 := checksum(outbuffer.Bytes())
 
-	log.Printf("check1 %#v check2 %#v\n", check1, check2)
+	mylogf("check1 %#v check2 %#v\n", check1, check2)
 
 	outbuffer.WriteByte(check1)
 	outbuffer.WriteByte(check2)
@@ -208,7 +277,7 @@ func checksum(data []byte) (byte, byte) {
 	var sum uint16 = 0
 
 	for i := 0; i < len(data); i++ {
-		//log.Printf("datai sum %v %v\n", data[i], sum)
+		//mylogf("datai sum %v %v\n", data[i], sum)
 		sum += uint16(data[i])
 	}
 
@@ -219,4 +288,18 @@ func checksum(data []byte) (byte, byte) {
 	check1 := byte((sum & 0xff00) >> 8)
 	check2 := byte(sum & 0x00ff)
 	return check1, check2
+}
+
+func writeLine(filename string, line string) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0660)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = f.WriteString(line)
+	if err != nil {
+		return err
+	}
+	return nil
 }
